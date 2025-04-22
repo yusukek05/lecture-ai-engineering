bc67f0acf1b663e322f6a0512c4f8e17c7a24637
Yusuke Kanzaki <yusuke.kanz@gmail.com>
2025-04-23 00:27:32 +0900

    モデルを変更

--- day1/03_FastAPI/app.py
+++ day1/03_FastAPI/app.py
@@ -13,7 +13,7 @@ from pyngrok import ngrok
 
 # --- 設定 ---
 # モデル名を設定
-MODEL_NAME = "google/gemma-2-2b-jpn-it"  # お好みのモデルに変更可能です
+MODEL_NAME = "google/gemma-3-1b-it"  # お好みのモデルに変更可能です
 print(f"モデル名を設定: {MODEL_NAME}")
 
 # --- モデル設定クラス ---
